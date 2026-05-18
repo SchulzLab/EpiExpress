09803c6f9a7d7f4524e7885072bd37287b61b84d
Shamim Ashrafiyan <shamim.ashrafiyan@gmail.com>
2026-05-18 12:38:09 +0200

    cleaning the code

--- Manuscript_Code/Plots/Disgenet_vs_found_sig_genes_final_including_CLL.R
+++ Manuscript_Code/Plots/Disgenet_vs_found_sig_genes_final_including_CLL.R
@@ -88,29 +88,34 @@ for (disease in top_diseases) {
   disease_genes <- AML_top_datasets %>% filter(Disease == disease) %>% pull(Gene)
   
   # Calculate raw p-values for each gene list
-  real_adj_pval <- calc_enrichment(real_genes, disease_genes, all_genes)
-  # unique_RF_adj_pval <- calc_enrichment(unique_RF, disease_genes, all_genes)
-  # unique_CNN_adj_pval <- calc_enrichment(unique_CNN, disease_genes, all_genes)
-  RF_p_adj_value <- calc_enrichment(RF_genes, disease_genes, all_genes)
-  CNN_p_adj_value <- calc_enrichment(CNN_genes, disease_genes, all_genes)
+  real_pval <- calc_enrichment(real_genes, disease_genes, all_genes)
+  RF_pval <- calc_enrichment(RF_genes, disease_genes, all_genes)
+  CNN_pval <- calc_enrichment(CNN_genes, disease_genes, all_genes)
   
-  # Store the -log10(adjusted p-values) in the enrichment_data dataframe
+  #clean
   enrichment_data <- rbind(enrichment_data, data.frame(
     Disease = disease,  
-    # unique_RF_adj = -log10(unique_RF_adj_pval + 1e-10),
-    # unique_CNN_adj = -log10(unique_CNN_adj_pval + 1e-10),
-    real_adj = -log10(real_adj_pval + 1e-10),
-    RF_adj = -log10(RF_p_adj_value + 1e-10),
-    CNN_adj = -log10(CNN_p_adj_value + 1e-10)
+    real_p = real_pval,
+    RF_p = RF_pval,
+    CNN_p = CNN_pval
   ))
+  
 }
 
 # Reshape the enrichment_data for plotting
 enrichment_data_melt <- enrichment_data %>%
   pivot_longer(cols = c(
-   # "unique_RF_adj", "unique_CNN_adj",
-    "real_adj", "RF_adj", "CNN_adj"),  
-               names_to = "List", values_to = "Enrichment")
+    "real_p", "RF_p", "CNN_p"),  
+               names_to = "List", values_to = "Raw_P")%>%
+mutate(List = case_match(List, 
+                           "real_p" ~ "real_adj", 
+                           "RF_p"   ~ "RF_adj", 
+                           "CNN_p"  ~ "CNN_adj"))
+
+#global Adj P-value:
+enrichment_data_melt <- enrichment_data_melt %>%
+  mutate(P_adj = p.adjust(Raw_P, method = "BH"),
+         Enrichment = -log10(P_adj + 1e-10))
 
 # Specify the desired order of the List factor
 enrichment_data_melt$List <- factor(enrichment_data_melt$List,  
@@ -118,10 +123,7 @@ enrichment_data_melt$List <- factor(enrichment_data_melt$List,
                                       # "unique_RF_adj", "unique_CNN_adj", 
                                       "real_adj", "RF_adj", "CNN_adj"))
 
-#global Adj P-value:
-enrichment_data_melt <- enrichment_data_melt %>%
-  mutate(P_adj = p.adjust(Raw_P, method = "BH"),
-         Enrichment = -log10(P_adj + 1e-10))
+
 
 #order
 enrichment_data_melt$List <- factor(enrichment_data_melt$List, 
